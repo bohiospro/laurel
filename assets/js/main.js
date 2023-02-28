69c803bc3dde4a2904b8557bb1350283d56159b8
bohiospro <joseinoa041331@gmail.com>
2023-02-28 12:24:50 -0400

    ijbuvfu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,61 +1,134 @@
-/*==================== SHOW MENU ====================*/
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
-
-    // Validate that variables exist
-    if(toggle && nav){
-        toggle.addEventListener('click', () =>{
-            // We add the show-menu class to the div tag with the nav__menu class
-            nav.classList.toggle('show-menu')
-        })
-    }
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
+window.addEventListener('scroll', scrollHeader)
 
-showMenu('nav-toggle','nav-menu')
-
-/*==================== SWIPER JS ====================*/
-let galleryThumbs = new Swiper('.gallery-thumbs', {
-    spaceBetween: 0,
-    slidesPerView: 0,
-})
-
-let galleryTop = new Swiper('.gallery-top', {
-    effect: 'fade',
+/*=============== SWIPER POPULAR ===============*/
+let swiperPopular = new Swiper(".popular__container", {
+    spaceBetween: 32,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
     loop: true,
 
-    thumbs: {
-      swiper: galleryThumbs
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+})
+
+/*=============== VALUE ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.value__accordion-item')
+
+// 1. Select each item
+accordionItems.forEach((item) =>{
+    const accordionHeader = item.querySelector('.value__accordion-header')
+
+    // 2. Select each header click
+    accordionHeader.addEventListener('click', () =>{
+        // 7. Create the tag
+        const openItem = document.querySelector('.accordion-open')
+        
+        // 5. Call toggle item function
+        toggleItem(item)
+
+        // 8. Check if the class exists
+        if(openItem && openItem!== item){
+            toggleItem(openItem)
+        }
+    })
+})
+
+// 3. Create a constant type function
+const toggleItem = (item) =>{
+    // 3.1 Create the tag
+    const accordionContent = item.querySelector('.value__accordion-content')
+
+    // 6. If there is another element that contains the class accordion-open remove its class
+    if(item.classList.contains('accordion-open')){
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    }else{
+        // 4. Add the maximum height of the content
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')
     }
-})
-
-
-/*==================== POPUP ====================*/
-const btnOpenVideo = document.querySelectorAll('.islands__video-content')
-const islandsPopup = document.getElementById('popup')
-
-function poPup(){
-    islandsPopup.classList.add('show-popup')
-}
-btnOpenVideo.forEach(b => b.addEventListener('click', poPup))
-
-const btnCloseVideo = document.getElementById('popup-close')
-
-btnCloseVideo.addEventListener('click', ()=> {
-    islandsPopup.classList.remove('show-popup')
-})
-
-/*==================== GSAP ANIMATION ====================*/
-const controlImg = document.querySelectorAll('.controls__img')
-
-function scrollAnimation(){
-    gsap.from('.islands__subtitle', {opacity: 0, duration: .2, delay: .2, y: -20})
-    gsap.from('.islands__title', {opacity: 0, duration: .3, delay: .3, y: -20})
-    gsap.from('.islands__description', {opacity: 0, duration: .4, delay: .4, y: -20})
-    gsap.from('.islands__button', {opacity: 0, duration: .5, delay: .5, y: -20})
-    gsap.from('.islands__video-content', {opacity: 0, duration: .6, delay: .6, y: -20})
-
-    islandsPopup.classList.remove('show-popup')
 }
 
-controlImg.forEach(c => c.addEventListener('click', scrollAnimation))
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal(`.home__title, .popular__container, .subscribe__container, .footer__container`)
+sr.reveal(`.home__description, .footer__info`, {delay: 500})
+sr.reveal(`.home__search`, {delay: 600})
+sr.reveal(`.home__value`, {delay: 700})
+sr.reveal(`.home__images`, {delay: 800, origin: 'bottom'})
+sr.reveal(`.logos__img`, {interval: 100})
+sr.reveal(`.value__images, .contact__content`, {origin: 'left'})
+sr.reveal(`.value__content, .contact__images`, {origin: 'right'})
